e4197e7a6a64ca95c44050ad425166182289d30b
Renzo <renzoangles@gmail.com>
2015-09-30 17:34:33 -0300

    primer push

---
+++ src/main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author renzo
+ */
+public class main {
+
+    public static void main(String[] args) {
+        Generador gen = new Generador();
+        gen.run(5); // 5 años de simulación
+    }
+    
+}
