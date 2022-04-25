fdd3c0708d872cc9fa9d3464f4e9d29fb65da750
BenjaminGonzalezF <80930564+BenjaminGonzalezF@users.noreply.github.com>
2022-04-24 22:58:53 -0400

    Update main.java

--- source-code/UnivDataGenerator/src/main.java
+++ source-code/UnivDataGenerator/src/main.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author renzo
+ * @author Benjamin G
  */
 public class main {
 
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("UnivDataGenerator permite generar archivos CSV con información de una universidad.");
+            System.out.println("UnivDataGenerator permite generar archivos CSV con información de una productora");
             System.out.println("Uso:");
-            System.out.println("java -jar UnivDataGenerator.jar N ");
-            System.out.println("Donde N es un número entero que indica el número de años de simulación.");
+            System.out.println("java -jar source-code.jar N ");
+            System.out.println("Donde N es un número entero que indica la cantidad de personas a generar.");
         } else {
             try {
                 int N = Integer.parseInt(args[0]);
